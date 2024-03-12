c6ff7e5fffde542bb1c6e70e63d37bc7b69d3446
linkfrg <link071209@gmail.com>
2024-03-13 00:15:07 +0500

    CLI

--- main.py
+++ main.py
@@ -1,6 +1,95 @@
-from crud import DatabaseManager
+from session import Session
+from db import db
 
-db = DatabaseManager()
-db.create_user('asd', 'super_passwd')
-db.add_user_password('asd', 'super_passwd', 'linkfrg', 'alololo')
-print(db.get_user_password('asd', 'super_passwd', 'linkfrg'))
+s = Session()
+
+
+def login():
+    username = input("enter username: ")
+    master_password = input("enter master password: ")
+    login_state = s.login(username=username, master_password=master_password)
+    if login_state == "SUCCESS_LOGIN":
+        print("You are successfully logined!")
+        menu()
+    elif login_state == "FAIL_LOGIN":
+        print("Wrong password or username! Try again.")
+        login()
+
+
+def register():
+    username = input("enter username: ")
+    master_password = input("enter master password: ")
+    reg_state = s.register(username=username, master_password=master_password)
+    if reg_state == "SUCCESS_REGISTER":
+        print("You are successfully registred!")
+        s.login(username, master_password)
+        menu()
+    elif reg_state == "USER_ALREADY_EXISTS":
+        print("User already exists! Try again.")
+        register()
+
+
+def add_password():
+    label = input("enter label: ")
+    login = input("enter login: ")
+    password = input("enter password: ")
+    s.add_password(label=label, login=login, password=password)
+
+
+def get_password():
+    label = input("enter label: ")
+    print(s.get_password(label=label))
+
+
+def list_passwords():
+    print(s.list_all_passwords())
+
+def delete_password():
+    label = input("enter label: ")
+    s.delete_password(label=label)
+
+
+def menu():
+    while True:
+        print("\nOptions:")
+        print("1. Add a password")
+        print("2. Get a password")
+        print("3. List all passwords")
+        print("4. Delete a password")
+        print("5. Exit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            add_password()
+        elif choice == "2":
+            get_password()
+        elif choice == "3":
+            list_passwords()
+        elif choice == "4":
+            delete_password()
+        elif choice == "5":
+            db.close()
+            exit(0)
+        else:
+            print("Invalid choice. Please try again.")
+
+
+def main():
+    print("1. Login")
+    print("2. Register")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        login()
+    elif choice == "2":
+        register()
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        db.close()
+        exit(0)
---
+++ session.py
@@ -0,0 +1,73 @@
+from db import db
+from cryptography.fernet import Fernet
+
+
+class Session:
+    def __init__(self) -> None:
+        self.__is_logined = False
+
+    def login(self, username: str, master_password: str) -> str:
+        db.execute("SELECT * FROM Users WHERE username = ?", (username,))
+        result = db.cursor.fetchone()
+        if result is not None:
+            if result[2] == master_password:
+                self.__encryption_key = result[3]
+                self.__username = username
+                self.__is_logined = True
+                return "SUCCESS_LOGIN"
+            else:
+                "FAIL_LOGIN"
+        else:
+            return "FAIL_LOGIN"
+            
+    def register(self, username: str, master_password: str) -> None:
+        db.execute("SELECT * FROM Users WHERE username = ?", (username,))
+        result = db.cursor.fetchone()
+
+        if result is None:
+            key = Fernet.generate_key()
+            db.execute(
+                "INSERT INTO Users (username, master_password, encryption_key) VALUES (?, ?, ?)",
+                (username, master_password, key),
+            )
+            db.execute(f"""
+                CREATE TABLE IF NOT EXISTS {username} (
+                id INTEGER PRIMARY KEY,
+                label TEXT NOT NULL,
+                login TEXT NOT NULL,
+                password TEXT NOT NULL
+                )
+                """)
+            return "SUCCESS_REGISTER"
+        else:
+            return "USER_ALREADY_EXISTS"
+
+    def add_password(self, label: str, login: str, password: str) -> None:
+        if self.__is_logined:
+            db.execute(f"SELECT * FROM {self.__username} WHERE login = ?", (login,))
+            data = db.cursor.fetchone()
+            if data is None:
+                cipher = Fernet(self.__encryption_key)
+                encrypted_password = cipher.encrypt(password.encode())
+                db.execute(
+                    f"INSERT INTO {self.__username} (label, login, password) VALUES (?, ?, ?)",
+                    (label, login, encrypted_password),
+                )
+
+    def get_password(self, label: str) -> str:
+        if self.__is_logined:
+            db.execute(f"SELECT * FROM {self.__username} WHERE label = ?", (label,))
+            data = db.cursor.fetchone()
+            if data:
+                encrypted_password = data[3]
+                cipher = Fernet(self.__encryption_key)
+                return cipher.decrypt(encrypted_password).decode()
+        
+    def list_all_passwords(self) -> list:
+        if self.__is_logined:
+            db.execute(f"SELECT * FROM {self.__username}")
+            return [(i[1], i[2], self.get_password(i[1])) for i in db.cursor.fetchall()]
+        
+    def delete_password(self, label: str) -> None:
+        if self.__is_logined:
+            db.execute(f"DELETE FROM {self.__username} WHERE label = ?", (label,))

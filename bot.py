df2623a194d13b952efef54a77ef75db658aed06
u1dm <vanyavana2010@gmail.com>
2024-03-12 21:15:59 +0200

    bot

--- bot.py
+++ bot.py
@@ -3,8 +3,8 @@ from telebot import types
 
 TOKEN='6707110127:AAGmMP890LBeAj9_J1jM65IU4P52T4ltif8'
 bot=telebot.TeleBot(TOKEN)
-# Словарь для хранения состояний пользователей
-user_states = {}
+
+user_passwords = {}
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
@@ -15,17 +15,50 @@ def start_message(message):
 
 @bot.callback_query_handler(func=lambda call: call.data == "menu")
 def menu_callback(call):
-    user_states[call.message.chat.id] = "waiting_password"
     bot.delete_message(call.message.chat.id, call.message.message_id)
-    bot.send_message(call.message.chat.id, "Please enter your password:")
+    markup = types.ReplyKeyboardMarkup(row_width=3, resize_keyboard=True)
+    itembtn1 = types.KeyboardButton('My passwords 📂')
+    itembtn2 = types.KeyboardButton('Lock 🔒')
+    markup.add(itembtn1, itembtn2)
+    bot.send_message(call.message.chat.id, "You are in the main menu, select an action 🔽", reply_markup=markup)
+@bot.message_handler(func=lambda message: True)
 
-@bot.message_handler(func=lambda message: user_states.get(message.chat.id) == "waiting_password")
-def handle_password(message):
-    if message.text.strip() == "1233":  
-        bot.send_message(message.chat.id, "Secret test")
-        user_states[message.chat.id] = None
-    else:
-        bot.send_message(message.chat.id, "Incorrect password. Please try again.")
-        user_states[message.chat.id] = None
+def echo_all(message):
+    if message.text == "My passwords 📂":
+        user_id = message.from_user.id
+        if user_id in user_passwords:
+            passwords = user_passwords[user_id]
+            passwords_text = "Your passwords 📄:\n"
+            for login, password in passwords:
+                passwords_text += f"- Login: {login}, Password: {password}\n"
+        else:
+            passwords_text = "You haven't added any passwords yet 🙊"
+        
+        markup = types.InlineKeyboardMarkup()
+        add_button = types.InlineKeyboardButton("Add 🔑", callback_data="add")
+        remove_button = types.InlineKeyboardButton("Remove 🗑", callback_data="remove")
+        markup.add(add_button, remove_button)
+        
+        bot.reply_to(message, passwords_text, reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda call: call.data == "add")
+def add_password_callback(call):
+    bot.delete_message(call.message.chat.id, call.message.message_id)
+    bot.send_message(call.message.chat.id, "Please enter the login 🙈")
+    bot.register_next_step_handler(call.message, process_login_step)
+
+def process_login_step(message):
+    user_id = message.from_user.id
+    login = message.text
+    bot.send_message(message.chat.id, "Please enter the password 🙈")
+    bot.register_next_step_handler(message, lambda msg: process_password_step(msg, login))
+
+def process_password_step(message, login):
+    user_id = message.from_user.id
+    password = message.text
+    if user_id not in user_passwords:
+        user_passwords[user_id] = []
+    user_passwords[user_id].append((login, password))
+    bot.send_message(message.chat.id, "Login and password added successfully 🙉")
 
 bot.infinity_polling()
